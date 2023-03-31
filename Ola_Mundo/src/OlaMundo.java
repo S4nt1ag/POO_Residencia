4eb05d8bbc8268b5b7d3fb1b11026106d14241ad
S4nt1ag <gabrielsantiagoneves2@gmail.com>
2023-03-30 23:03:38 -0300

    atualização POO residencia

---
+++ Ola_Mundo/src/OlaMundo.java
@@ -0,0 +1,9 @@
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+
+	}
+
+}
---
+++ exercicio2/src/exercicio2/BalancoTrimestral.java
@@ -0,0 +1,17 @@
+package exercicio2;
+
+public class BalancoTrimestral {
+
+	public static void main(String[] args) {
+		var gastosJaneiro = 15000;
+		var gastosFevereiro = 23000;
+		var gastosMarco = 17000;
+		var gastosTrimestre = (gastosJaneiro+gastosFevereiro+gastosMarco);
+		
+		System.out.println(gastosTrimestre);
+		
+		
+
+	}
+
+}
