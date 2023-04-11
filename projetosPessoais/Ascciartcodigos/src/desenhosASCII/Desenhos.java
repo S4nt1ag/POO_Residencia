1ee8227ac80039cb6677cd398c561537f1ab7972
Santiago <gabrielsantiagoneves2@gmail.com>
2023-04-11 19:03:34 -0300

    Atualização 11/04/2023 19:03

---
+++ projetosPessoais/Ascciartcodigos/src/desenhosASCII/Desenhos.java
@@ -0,0 +1,28 @@
+package desenhosASCII;
+
+public class Desenhos {
+
+	
+		public void   DesenhoCoelho() {
+			System.out.println("             ,");
+			System.out.println("            /|      __");
+			System.out.println("           / |   ,-~ /");
+			System.out.println("          Y :|  //  /");
+			System.out.println("          | jj /( .^");
+			System.out.println("          >-\"~\"-v\"");
+			System.out.println("         /       Y");
+			System.out.println("        jo  o    |");
+			System.out.println("       ( ~T~     j");
+			System.out.println("        >._-' _./");
+			System.out.println("       /   \"~\"  |");
+			System.out.println("      Y     _,  |");
+			System.out.println("     /| ;-\"~ _  l");
+			System.out.println("    / l/ ,-\"~    \\");
+			System.out.println("    \\//\\/      .- \\");
+			System.out.println("     Y        /    Y");
+			System.out.println("     l       I     !");
+			System.out.println("     ]\\      _\\    /\"\\");
+			System.out.println("    (\" ~----( ~   Y.  )");
+			System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		}
+}
---
+++ projetosPessoais/Ascciartcodigos/src/desenhosASCII/PortifolioUsarDepois.java
@@ -0,0 +1,10 @@
+package desenhosASCII;
+
+public class PortifolioUsarDepois {
+
+    public static void main(String[] args) {
+    	Desenhos ds = new Desenhos();
+    	ds.DesenhoCoelho();
+    }
+}
+
