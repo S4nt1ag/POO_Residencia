5024ad5a978291bcb92bda21d8e571042159878b
Santiago <gabrielsantiagoneves2@gmail.com>
2023-04-20 16:02:38 -0300

    urna

---
+++ projetosPessoais/hashmap Votacao/src/app/Program.java
@@ -0,0 +1,54 @@
+package app;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		Map<String, Integer> map = new HashMap<>();
+		
+		System.out.print("Digite o caminho do arquivo: ");
+		String path = sc.nextLine();
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+			String line = br.readLine();
+			while (line != null) {
+
+				// processar a linha
+				
+				String[] fields = line.split(",");
+				String name = fields[0];
+				Integer votes = Integer.parseInt(fields[1]);
+				
+				if (map.get(name)== null) {
+					
+					map.put(name, votes);
+				} else {
+					map.put(name, votes + map.get(name));
+				}
+				
+				
+				
+				line = br.readLine();
+			}
+			for (String key : map.keySet()) {
+				System.out.println(key + ": " + map.get(key));
+			}
+
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+
+		sc.close();
+
+	}
+}
